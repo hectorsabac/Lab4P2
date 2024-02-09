1b0cbae6356462585fef00813158a5c91242a327
hectorsabac <147878929+hectorsabac@users.noreply.github.com>
2024-02-09 14:05:14 -0600

    Update JuegoAhorcadoBase.java

--- Game/src/game/JuegoAhorcadoBase.java
+++ Game/src/game/JuegoAhorcadoBase.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package game;
 
-/**
- *
- * @author hecto
- */
 public abstract class JuegoAhorcadoBase implements JuegoAhorcado{
     protected String palabraSecreta;
     protected String palabraActual;
