ffea538bb6d31348a67a4dbb7b98d6f04532d1c7
hectorsabac <147878929+hectorsabac@users.noreply.github.com>
2024-02-09 14:04:03 -0600

    Update JuegoAhorcado.java

--- Game/src/game/JuegoAhorcado.java
+++ Game/src/game/JuegoAhorcado.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package game;
 
-/**
- *
- * @author hecto
- */
 public interface JuegoAhorcado {
-    String inicializarPalabraSecreta();
+    void inicializarPalabraSecreta();
     void jugar();
 }
